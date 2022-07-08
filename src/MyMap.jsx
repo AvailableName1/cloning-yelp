e9049e1d9e5a424926567fd61d667f24ed2a1d0c
AvailableName1 <liker1011@gmail.com>
2022-07-08 20:08:54 +0400

    updated ReadMe as well as MyMap with async fetching and some apikeys imported from the file now mentioned in ignoring rules

--- src/MyMap.jsx
+++ src/MyMap.jsx
@@ -1,13 +1,28 @@
-import { Map } from "pigeon-maps"
+import { Map, Marker} from "pigeon-maps"
 import { maptiler } from 'pigeon-maps/providers'
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { apiKeyYandexPlaces, apiKeyMapTiler } from "./securePlace"
 // import css
 
-export default function MyMap() {
-  const [center, setCenter] = useState([41.6941, 44.8337])
-  const [zoom, setZoom] = useState(11)
-  const maptilerProvider = maptiler('QUSx1i2HvrLQCCvXImBD', 'streets')
 
+export default function MyMap() {
+  const [center, setCenter] = useState([41.704200, 44.789464]);
+  const [zoom, setZoom] = useState(16);
+  const [data, setData] = useState(null)
+
+  const maptilerProvider = maptiler(apiKeyMapTiler, 'streets');
+
+  useEffect(() => {
+    getYandexPlaces(`coffee and restaurants`, `en_US`)
+    .then( places => {
+      setData(places);
+      places.features.map(place => {
+        console.log(place.geometry.coordinates);
+      });
+    })
+    return () => {};
+  }, [])
+  
   return (
     <Map
     provider={maptilerProvider}
@@ -18,6 +33,30 @@ export default function MyMap() {
       setCenter(center) 
       setZoom(zoom) 
     }}
-    />
+    >
+      {data && data.features.map(place => <Marker width={50} anchor={place.geometry.coordinates} />)}
+    </Map>
   )
+
+  // we're going to get Places by this function
+  async function getYandexPlaces(searchText, responseLanguage) {
+    let url = createRequestURL(searchText, responseLanguage);
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      return data;
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  function createRequestURL(searchText, responseLanguage) {
+    let url = new URL(`https://search-maps.yandex.ru/v1/`);
+    url.searchParams.set('apikey', apiKeyYandexPlaces);
+    url.searchParams.set('text', searchText);
+    url.searchParams.set('lang', responseLanguage);
+    url.searchParams.set('ll', center.toString());
+    url.searchParams.set('spn', `0.05,0.05`);
+    return url
+  }
 }
