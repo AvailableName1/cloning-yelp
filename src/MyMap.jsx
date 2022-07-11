f2df264f9061b1bd489aba3da91ed2fcbfa23f38
AvailableName1 <liker1011@gmail.com>
2022-07-11 13:30:16 +0400

    Added functinality: maps display places on the map, info popups on mouse hover or mobile touch. Maps loads new data on view change

--- src/MyMap.jsx
+++ src/MyMap.jsx
@@ -1,42 +1,68 @@
-import { Map, Marker} from "pigeon-maps"
-import { maptiler } from 'pigeon-maps/providers'
-import { useState, useEffect } from "react"
-import { apiKeyYandexPlaces, apiKeyMapTiler } from "./securePlace"
+import { Map, Marker } from "pigeon-maps";
+import { maptiler } from "pigeon-maps/providers";
+import { CSSTransition } from "react-transition-group";
+import { useState, useEffect } from "react";
+import { apiKeyYandexPlaces, apiKeyMapTiler } from "./securePlace";
+import PopUp from "./components/PopUp";
 // import css
 
-
 export default function MyMap() {
-  const [center, setCenter] = useState([41.704200, 44.789464]);
-  const [zoom, setZoom] = useState(16);
-  const [data, setData] = useState(null)
+  const [center, setCenter] = useState([41.7042, 44.789464]);
+  const [zoom, setZoom] = useState(17);
+  const [data, setData] = useState(null);
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [displayed, setDisplayed] = useState(null);
 
-  const maptilerProvider = maptiler(apiKeyMapTiler, 'streets');
+  const maptilerProvider = maptiler(apiKeyMapTiler, "streets");
 
   useEffect(() => {
-    getYandexPlaces(`coffee and restaurants`, `en_US`)
+    getYandexPlaces(`food`, `en_US`)
     .then( places => {
       setData(places);
-      places.features.map(place => {
-        console.log(place.geometry.coordinates);
-      });
-    })
-    return () => {};
-  }, [])
-  
+      console.log(places.features);
+    })}, [center])
+
   return (
     <Map
-    provider={maptilerProvider}
-    height={'80vh'}
-    defaultCenter={center}
-    defaultZoom={zoom}
-    onBoundsChanged={({ center, zoom }) => { 
-      setCenter(center) 
-      setZoom(zoom) 
-    }}
+      provider={maptilerProvider}
+      height={"80vh"}
+      twoFingerDrag={true}
+      defaultCenter={center}
+      defaultZoom={zoom}
+      onBoundsChanged={({ center, zoom }) => {
+        setCenter(center);
+        setZoom(zoom);
+      }}
     >
-      {data && data.features.map(place => <Marker width={50} anchor={place.geometry.coordinates} />)}
+      <CSSTransition
+      in={showPopUp}
+      timeout={200}
+      classNames="popUp"
+      mountOnEnter
+      unmountOnExit
+      >
+        <PopUp 
+        title={displayed ? displayed.name : ''}
+        type="Cafe/Restaurant"
+        text={displayed ? displayed.address : ''}
+        />
+      </CSSTransition>
+      {data &&
+        data.features.map((place) => (
+          <Marker
+            key={place.properties.CompanyMetaData.id}
+            width={50}
+            anchor={[...place.geometry.coordinates].reverse()}
+            onMouseOver={(e) => {
+              setDisplayed(place.properties.CompanyMetaData);
+              setShowPopUp(true);
+            }}
+            onMouseOut={() => setShowPopUp(false)}
+            // onClick={}
+          />
+        ))}
     </Map>
-  )
+  );
 
   // we're going to get Places by this function
   async function getYandexPlaces(searchText, responseLanguage) {
@@ -45,18 +71,23 @@ export default function MyMap() {
       let response = await fetch(url);
       let data = await response.json();
       return data;
-    } catch(e) {
+    } catch (e) {
       console.log(e);
     }
   }
 
   function createRequestURL(searchText, responseLanguage) {
+    let requestCenter = [...center];
     let url = new URL(`https://search-maps.yandex.ru/v1/`);
-    url.searchParams.set('apikey', apiKeyYandexPlaces);
-    url.searchParams.set('text', searchText);
-    url.searchParams.set('lang', responseLanguage);
-    url.searchParams.set('ll', center.toString());
-    url.searchParams.set('spn', `0.05,0.05`);
-    return url
+    url.searchParams.set("apikey", apiKeyYandexPlaces);
+    url.searchParams.set("text", searchText);
+    url.searchParams.set("lang", responseLanguage);
+    url.searchParams.set("type", "biz");
+    url.searchParams.set("ll", requestCenter.reverse().toString());
+    url.searchParams.set("spn", `0.005,0.005`);
+    url.searchParams.set("rspn", `1`);
+    url.searchParams.set("results", `25`);
+    console.log(url);
+    return url;
   }
 }
