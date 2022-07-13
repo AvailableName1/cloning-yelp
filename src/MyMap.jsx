d92eb2466dbf1f3d777d80292e435fadac9081e2
AvailableName1 <liker1011@gmail.com>
2022-07-13 17:15:29 +0400

    Data is now managed in the App component and translates to routes cafe and restaurant using OutletContext. Besides, added functionality is that app now displays the list of cafes/restaurants when you click on such link in the header.

--- src/MyMap.jsx
+++ src/MyMap.jsx
@@ -1,36 +1,27 @@
 import { Map, Marker } from "pigeon-maps";
 import { maptiler } from "pigeon-maps/providers";
 import { CSSTransition } from "react-transition-group";
-import { useState, useEffect } from "react";
-import { apiKeyYandexPlaces, apiKeyMapTiler } from "./securePlace";
+import { useState } from "react";
 import PopUp from "./components/PopUp";
 // import css
 
-export default function MyMap() {
-  const [center, setCenter] = useState([41.7042, 44.789464]);
+export default function MyMap({ centerChanger, ...props }) {
   const [zoom, setZoom] = useState(17);
-  const [data, setData] = useState(null);
   const [showPopUp, setShowPopUp] = useState(false);
   const [displayed, setDisplayed] = useState(null);
 
-  const maptilerProvider = maptiler(apiKeyMapTiler, "streets");
-
-  useEffect(() => {
-    getYandexPlaces(`food`, `en_US`)
-    .then( places => {
-      setData(places);
-      console.log(places.features);
-    })}, [center])
+  const mapTilerAPI = process.env.REACT_APP_API_MAPTILER;
+  const maptilerProvider = maptiler(mapTilerAPI, "streets");
 
   return (
     <Map
       provider={maptilerProvider}
       height={"80vh"}
       twoFingerDrag={true}
-      defaultCenter={center}
+      defaultCenter={props.center}
       defaultZoom={zoom}
       onBoundsChanged={({ center, zoom }) => {
-        setCenter(center);
+        centerChanger(center);
         setZoom(zoom);
       }}
     >
@@ -47,8 +38,8 @@ export default function MyMap() {
         text={displayed ? displayed.address : ''}
         />
       </CSSTransition>
-      {data &&
-        data.features.map((place) => (
+      {props.data &&
+        props.data.features.map((place) => (
           <Marker
             key={place.properties.CompanyMetaData.id}
             width={50}
@@ -64,30 +55,5 @@ export default function MyMap() {
     </Map>
   );
 
-  // we're going to get Places by this function
-  async function getYandexPlaces(searchText, responseLanguage) {
-    let url = createRequestURL(searchText, responseLanguage);
-    try {
-      let response = await fetch(url);
-      let data = await response.json();
-      return data;
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  function createRequestURL(searchText, responseLanguage) {
-    let requestCenter = [...center];
-    let url = new URL(`https://search-maps.yandex.ru/v1/`);
-    url.searchParams.set("apikey", apiKeyYandexPlaces);
-    url.searchParams.set("text", searchText);
-    url.searchParams.set("lang", responseLanguage);
-    url.searchParams.set("type", "biz");
-    url.searchParams.set("ll", requestCenter.reverse().toString());
-    url.searchParams.set("spn", `0.005,0.005`);
-    url.searchParams.set("rspn", `1`);
-    url.searchParams.set("results", `25`);
-    console.log(url);
-    return url;
-  }
+  
 }
--- src/routes/Caffee.jsx
+++ src/routes/Caffee.jsx
@@ -1,7 +1,33 @@
+import { useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
+
 export default function Caffee() {
-    return (
-        <main>
-            <h1>Caffee</h1>
-        </main>
-    );
+  const [data, setRequestText] = useOutletContext();
+
+  useEffect(() => {
+    setRequestText("cafe");
+    return () => {};
+  }, []);
+
+  return (
+    <section>
+      <h1>Cafe</h1>
+      <div className="infoCards">
+        {data.features.map((place) => {
+          return (
+            <div className="card" key={place.properties.CompanyMetaData.id}>
+              <div className="card-body">
+                <h6 className="card-title">
+                  {place.properties.CompanyMetaData.name}
+                </h6>
+                <p className="card-text">
+                  {place.properties.CompanyMetaData.address}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
